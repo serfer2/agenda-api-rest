7a8c0d49af0c10f40868be6fadc2c2c64d95e078
Sergio Fernández <serfer2@gmail.com>
2018-09-25 21:06:05 +0200

    refactorizada la app para poder crear Note con POST directamente leyendo JSON con express

--- server/server.js
+++ server/server.js
@@ -1,25 +1,29 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-var nextDay = require('./utils').nextDay;
+var { User, userSchema } = require('./models/user');
+var { Note, noteSchema } = require('./models/note');
+var { mongoose } = require('./db/mongoose');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Agenda', { useNewUrlParser: true });
+var app = express();
 
-var noteSchema = mongoose.Schema({
-    text: {
-        type: String
-    },
-    date: {
-        type: Date,
-        default: nextDay
-    },
-    readed: {
-        type: Boolean
-    },
-    readedAt: {
-        // timestamp
-        type: Number
-    }
+// Usamos middleware para que todo lo que llegue en Body
+// de las request sea convertido a objeto JSON al vuelo
+//
+app.use(bodyParser.json());
+
+app.post('/notes', (req, res) => {
+    var nota = new Note(req.body);
+    nota.save().then(
+        (doc) => {
+            res.status(201).send(doc);
+        },
+        (err) => {
+            res.status(400).send(err);
+        }
+    );
 });
 
-var Note = mongoose.model('Note', noteSchema);
+app.listen(3000, () => {
+    console.log('Escuchando en el puerto 3000');
+});
